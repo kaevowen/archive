c631457d269e2e41062511975a594a806bd7fae0
kaevowen <kaev.owen@gmail.com>
2020-01-29 20:57:33 +0900

    meh

---
+++ boj/10872.py
@@ -0,0 +1,7 @@
+def factorial(n):
+    fact = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 
+    362880, 3628800, 39916800, 479001600]
+    return fact[n]
+
+n = int(input())
+print(factorial(n))
---
+++ boj/1427.py
@@ -0,0 +1,4 @@
+n = list(map(int, input()))
+n.sort(reverse=True)
+for n in n:
+	print(n, end='')
