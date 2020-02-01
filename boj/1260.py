bea5c6e992cd1cd26228e3801569cabe872d11d7
kaevowen <kaev.owen@gmail.com>
2020-02-01 13:52:38 +0900

    meh

--- boj/1260.py
+++ boj/1260.py
@@ -6,18 +6,10 @@ def BFS(graph, root):
 		vertex = q.pop(0)
 		if vertex not in visited:
 			visited.append(vertex)
-<<<<<<< HEAD
-			q+=sorted(list(graph_list[vertex] - set(visited)))
-
-	return visited
-
-=======
 			q+=sorted(list(graph[vertex] - set(visited)))
 
 	return visited
 
-
->>>>>>> 492e57d7f7d40660fd323f3d7521924eb71627c9
 def DFS(graph, root):
 	visited = []
 	stack = [root]
@@ -26,9 +18,7 @@ def DFS(graph, root):
 		node = stack.pop()
 		if node not in visited:
 			visited.append(node)
-<<<<<<< HEAD
-			stack += sorted(list(graph_list[node] - set(visited))
-				, reverse = True)
+			stack += list(graph[node] - set(visited))
 	return visited
 
 def DFS_recursive(graph, start, end, visited=[]):
@@ -46,42 +36,19 @@ def DFS_recursive(graph, start, end, visited=[]):
 #N = 정점의 개수
 #M = 간선의 개수
 #V = 인덱스(시작) 번호
-=======
-			stack += list(graph[node] - set(visited))
-	return visited
 
-adj_set = {
-	1:set([5,4]),
-	2:set([3,1]),
-	3:set([3,2]),
-	4:set([4,3]),
-	5:set([5,3]),
-}
-
-for i in list(DFS(adj_set, 1)):
-	print(i)
-'''
->>>>>>> 492e57d7f7d40660fd323f3d7521924eb71627c9
-N, M, V = map(int, input().split())
+N, M = map(int, input().split())
 graph_list = [set([]) for _ in range(N+1)]
 for _ in range(M):
 	i, j = map(int,input().split())
 	graph_list[i].add(j)
 	graph_list[j].add(i)
 
-<<<<<<< HEAD
+
 print(graph_list)
-=======
->>>>>>> 492e57d7f7d40660fd323f3d7521924eb71627c9
 
-for i in list(DFS(graph_list, V)):
-	print(i, end=' ')
-print()
+for i in range(len(graph_list)):
+	for j in list(BFS(graph_list, i)):
+		print(j, end=' ')
+	print()
 
-for j in list(BFS(graph_list, V)):
-<<<<<<< HEAD
-	print(j, end=' ')	
-=======
-	print(j, end=' ')	
-'''
->>>>>>> 492e57d7f7d40660fd323f3d7521924eb71627c9
--- boj/1325.py
+++ boj/1325.py
@@ -1,30 +1,41 @@
-def DFS(v):
+import sys
+from collections import deque
+
+def BFS(v):
+    q = deque()
+    visited = [False] * (n+1)
+    q.append(v)
     visited[v] = True
-    print(v, ' ',end='')
-    for w in adj_list[v]:
-        if not visited[w]:
-            DFS(w)
+    cnt = 1
 
-'''
-N, M = map(int, input().split())
-graph_list = [set([]) for _ in range(N+1)]
+    while q:
+        v = q.popleft()
+        for e in adj[v]:
+            if not (visited[e]):
+                q.append(e)
+                visited[e] = True
+                cnt += 1
+    return cnt
+
+n, M = map(int, sys.stdin.readline().split())
+adj = [[] for i in range(n+1)]
+max_num = []
+m = -1
+
 for _ in range(M):
-    i, j = map(int,input().split())
-    graph_list[i].add(j)
-    graph_list[j].add(i)
+    a, b = map(int, sys.stdin.readline().split())
+    adj[b].append(a)
 
-print(graph_list)
-for i in range(1, M+1):
-    for j in list(DFS(graph_list, i)):
-        print(j, end=' ')
-    print()
-'''
+for i in range(1, n+1):
+    c = BFS(i)
 
-adj_list = [[],[3],[3],[4,5],[],[]]
-N = len(adj_list)
-visited = [False for _ in range(N)]
+    if c == m:
+        max_num.append(i)
+        m = c
+    elif c > m:
+        max_num = [i]
+        m = c
 
-for j in range(1, N):
-    if not visited[j]:
-        DFS(j)
-
+for e in max_num:
+    print(e, end = ' ')
+print()
