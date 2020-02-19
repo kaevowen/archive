5ce1ec43df24b636fd2711f9618cf7d30de5d9a6
kaevowen <kaev.owen@gmail.com>
2020-02-19 15:20:22 +0900

    meh

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('shareRes.urls')),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('todolist/', include('todolist.urls')),
+    path('sendEmail/', include('sendEmail.urls')),
+
 ]
